6995876c4b7e55ad2c2e71bb4b3cb9d94239f983
XiaochenChen <cxc0520@hotmail.com>
2014-10-23 16:36:23 -0400

    Added __init__.py forpackage import

---
+++ classes/__init__.py
@@ -0,0 +1,5 @@
+'''__init__.py can be empty, as long as it exists, the whole dirctory should be 
+regarded as a package when imported.
+
+And __init__.py could also restrict the from xxx import *. By __all__ = [a, b, c, d]
+'''
